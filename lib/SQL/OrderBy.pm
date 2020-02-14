f0d75994bfe007027f1ad5075cb62af4338b0536
Gene Boggs <gene@ology.net>
2020-02-14 06:42:36 -0800

    Drop weaver added sections

--- lib/SQL/OrderBy.pm
+++ lib/SQL/OrderBy.pm
@@ -337,27 +337,4 @@ function uses those by default.
 * The optional arguments are described in the toggle_resort function
 documentation, above.
 
-=head1 DEPENDENCIES
-
-None.
-
-=head1 EXPORTS
-
-None, Nothing, Nada.
-
-=head1 TO DO
-
-Add functions for different kinds of resorting.
-
-=head1 AUTHOR
-
-Gene Boggs E<lt>gene@cpan.orgE<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright 2003 by Gene Boggs
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself. 
-
 =cut
